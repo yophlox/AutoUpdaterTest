79c4fb920f4f08f420150da516e764d2a28371b5
Phlox <168133165+yophlox@users.noreply.github.com>
2024-10-12 10:47:11 -0400

    we chilly, it works properly

--- source/import.hx
+++ source/import.hx
@@ -1,2 +1,1 @@
-import backend.util.Util;
-import backend.util.Cache;
+import backend.AutoUpdater;
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,16 +1,24 @@
 package states;
 
 import flixel.FlxState;
+import flixel.FlxG;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		#if (desktop && !debug)
+		AutoUpdater.checkForUpdates();
+		#end
 	}
 
 	override public function update(elapsed:Float)
 	{
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			trace(AutoUpdater.CURRENT_VERSION);
+		}
 		super.update(elapsed);
 	}
 }
